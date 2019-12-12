d6de27e523520203fa77cb5addda68e12ce99320
Nilesh <Nilesh Verma@diasparkindia.com>
2019-12-12 11:46:15 +0530

    updated

--- controller/mongoController.js
+++ controller/mongoController.js
@@ -38,28 +38,6 @@ module.exports = {
       res.send(errors.errors[0].msg)
     } else {
       forAddRepos.reposAdded(loginname,reposname,html_url,res)
-      // const userdetail = userDetailsModel.find({ login: loginname })
-      // await userdetail.exec((err, data) => {
-      //   if (err) throw err
-      //   if (!data.length) { res.send('User not find') } else {
-      //     const repos = reposModel.find({ login: loginname, name: reposname })
-      //     repos.exec((err, result) => {
-      //       if (err) throw err
-      //       if (result.length) res.send('Repository Name Exist')
-      //       else {
-      //         const reposDetails = new reposModel({
-      //           login: loginname,
-      //           name: reposname,
-      //           html_url: html_url
-      //         })
-      //         reposDetails.save((err, data) => {
-      //           if (err) throw err
-      //           res.send('Repository Added!')
-      //         })
-      //       }
-      //     })
-      //   }
-      // })
     }
   },
 
